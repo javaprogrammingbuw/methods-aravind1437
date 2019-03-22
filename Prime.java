62f49947e066dc3642a8996663284fd2495c8772
aravind1437 <44303218+aravind1437@users.noreply.github.com>
2019-03-22 14:54:17 +0530

    please check with the assignment

--- Prime.java
+++ Prime.java
@@ -2,14 +2,55 @@ import java.util.Scanner;
 
 
 public class Prime {
+	
+	public static void main(String[] args) {
 		
-	//todo: write a method isPrime which tells you if a given number is a prime or not
-	//then, write another method findDoublette which takes an integer value as input and 
-	//is looking for the first prime-doublette it findes. A prime-doublette consists of
-	//two prime values with the difference of 2 (e.g. 3 and 5).
-	//The function should return the smaller one of the doublette. If there exists no
-	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
-	//the function should return 0.
+		
 		
 	
-}
+		
+	    int i;
+		int a;
+		int count = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the Valve of a");
+		a= scan.nextInt();
+		if(a==0 || a==1) {
+			System.out.println("Not a prime number");
+		}
+		else {
+			
+		for(i=2; i<a; i++) {
+				if(a%i == 0) {
+					count++;
+					break;
+			
+				}
+			}
+		if (count == 0) {
+			System.out.println(" this is a prime number");
+		}else {
+			System.out.println(" this is not a prime number");
+		}
+	}
+	
+	}
+	/*public static void doublette() {
+		int a;
+		int x = a+2;
+		while(x%2 == 0) {
+			System.out.println("sfs");*/
+		
+	
+	
+		
+	
+	
+			
+		}
+		
+	
+
+	
+
