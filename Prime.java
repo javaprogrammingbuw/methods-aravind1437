bea14d94d0967cd8972c7769d6346b0c13dadeb6
aravind1437 <44303218+aravind1437@users.noreply.github.com>
2019-03-28 13:53:27 +0530

    please check with the modified assignment

--- Prime.java
+++ Prime.java
@@ -2,59 +2,90 @@ import java.util.Scanner;
 
 
 public class Prime {
-	
-	public static void main(String[] args) {
-		
-		
-		
-	
-		
-	    int i;
-		int a;
-		int count = 0;
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Enter the Valve of a");
-		a= scan.nextInt();
 
-		//This should have been solved in another method
-		//Doublette missing (not complete)
 
-		if(a==0 || a==1) {
-			System.out.println("Not a prime number");
+	//todo: write a method isPrime which tells you if a given number is a prime or not
+	//then, write another method findDoublette which takes an integer value as input and 
+	//is looking for the first prime-doublette it findes. A prime-doublette consists of
+	//two prime values with the difference of 2 (e.g. 3 and 5).
+	//The function should return the smaller one of the doublette. If there exists no
+	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
+	//the function should return 0.
+
+	public static void main(String[] args){
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println(" Enter the number ");
+		
+		int n = scan.nextInt();
+		
+		boolean a = CheckPrime(n);
+		
+		if(a==true){
+			
+			System.out.println("The given number is a prime number");
+		}else{
+			
+			System.out.println("The given number is not a prime number");
 		}
-		else {
-			
-		for(i=2; i<a; i++) {
-				if(a%i == 0) {
-					count++;
-					break;
-			
+		
+		System.out.println("The smallest number is " + findDoublette(n));
+		
+	}
+
+	public static boolean CheckPrime(int a){
+		
+		int count =0;
+		
+		for(int i=1;i<a-1;i++){
+		
+			if(a%i==0){
+				
+				count = count +1;
+				
+				if(count >1){
+				
+					return false;
 				}
+				
 			}
-		if (count == 0) {
-			System.out.println(" this is a prime number");
-		}else {
-			System.out.println(" this is not a prime number");
-		}
-	}
-	
-	}
-	/*public static void doublette() {
-		int a;
-		int x = a+2;
-		while(x%2 == 0) {
-			System.out.println("sfs");*/
-		
-	
-	
-		
-	
-	
+				
 			
 		}
+		return true;
+	}
+	public static int findDoublette(int a){
+		 
+		int count = 0;
+		
+		for(int x = a+1; x<Integer.MAX_VALUE; x++){
+			
+			boolean b = CheckPrime(x);
+			     
+				if(b == true && count<1){
+                    
+					boolean c = CheckPrime(x+2);
+                     
+					if(c == true){
+                     	
+						int y=x+2;
+                 
+						System.out.println("The nearest prime-doublette numbers are  "+x+" and " + y);
+                        
+						count = count++;
+                        
+						return x;
+                     }
+
+				}
+					
+				}
+		return 0;
+
+	}
+	
+	
 		
 	
-
-	
-
+}
